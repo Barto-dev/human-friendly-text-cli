ddab721962e3de9040ac43c201279de2deda57a5
Illia <illya1004work@gmail.com>
2022-05-29 20:28:31 +0100

    feat: added helpers

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,3 +1,79 @@
+import { hoursDescription, minutesDescription } from '../config';
+
 export const capitalizeFirstLetter = (string: string) => {
   return string.charAt(0).toUpperCase() + string.slice(1);
 };
+
+export const getHumanReadableNumberValue = (value: string, variant: 'hours' | 'minutes'): string => {
+  if (variant === 'hours') {
+    return hoursDescription[value];
+  }
+
+  return minutesDescription[value];
+};
+
+const getNextHour = (hour: string): string => {
+  if (hour === '00') {
+    return '1';
+  }
+
+  if (hour === '23') {
+    return '00'
+  }
+
+  const nextHour = Number(hour) + 1;
+  return nextHour.toString();
+}
+
+// string matching check 00:00 - 23:59
+export const checkTimeArg = (time: string) => {
+  const regEx = /^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$/;
+  return regEx.test(time);
+}
+
+// Get time in format h:m (ex. 11:30, 12:56)
+export const getHumanFriendlyTime = (time: string) => {
+  if (!checkTimeArg(time)) {
+    return 'Enter a valid time, for example 11:30, 1:42, 12:15'
+  }
+
+  const timesInArr = time.split(':');
+  const hours = timesInArr[0];
+  const minutes = timesInArr[1];
+  let humanReadableHours = getHumanReadableNumberValue(hours, 'hours');
+  let humanReadableMinutes = getHumanReadableNumberValue(minutes, 'minutes');
+  // convert minute from format '05' => 5
+  const minuteStrToNumber = parseInt(minutes);
+  // if number didn't multiple we show message 14.02 => two minutes past two
+  const isNumberMultipleFive = minuteStrToNumber % 5 === 0;
+
+
+  if (minutes === '00') {
+    return `${humanReadableHours} o'clock`;
+  }
+
+  if (minutes === '30') {
+    return `Half past ${humanReadableHours}`;
+  }
+
+
+  if (minuteStrToNumber >= 1 && minuteStrToNumber < 30) {
+    const minuteDescription = isNumberMultipleFive ? ' ' : ' minutes ';
+    return `${humanReadableMinutes}${minuteDescription}past ${humanReadableHours}`
+  }
+
+  if (Number(minutes) > 30 && Number(minutes) <=59) {
+    const minuteDescription = isNumberMultipleFive ? ' ' : ' minutes ';
+    const minutesToEndHour = 60 - minuteStrToNumber;
+    // if we get 1 to end of our we transform this to '01' for get human-readable value
+    const minuteToStr = minutesToEndHour < 10 ? `0${minutesToEndHour}` : `${minutesToEndHour}`;
+    const nextHour = getNextHour(hours);
+    humanReadableMinutes = getHumanReadableNumberValue(minuteToStr, 'minutes');
+    humanReadableHours = getHumanReadableNumberValue(nextHour, 'hours');
+    return `${humanReadableMinutes}${minuteDescription}to ${humanReadableHours}`
+  }
+
+  if (time) {
+    return time;
+  }
+}

2b8ea38de28b94481a66197f15b2db7c73d91a3e
Illia <illya1004work@gmail.com>
2022-06-04 13:27:49 +0100

    wip: deleted emite files

--- src/cli/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var commander_1 = require("commander");
-var time_1 = require("./commands/time");
-commander_1.program.addCommand(time_1.time);
-commander_1.program.parse(process.argv);
